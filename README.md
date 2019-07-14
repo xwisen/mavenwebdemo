cc88ca44df985acf9680d303e8b29d5a8873e5a9
共鼓 <xwisen.wz@antfin.com>
2019-07-14 20:26:54 +0800

    增加README.md

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [http://how2j.cn/k/idea/idea-maven-web/1356.html](http://how2j.cn/k/idea/idea-maven-web/1356.html)
 
 [javax.servlet.annotation不存在](https://blog.csdn.net/wust_lh/article/details/73469789)
+
+[idea mvn 生成不了war ](https://www.cnblogs.com/LEEEEEASON/p/7131102.html)
--- src/main/java/com/xwisen/mavenwebdemo/Default.java
+++ src/main/java/com/xwisen/mavenwebdemo/Default.java
@@ -1,24 +1,24 @@
 package com.xwisen.mavenwebdemo;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import java.util.logging.Logger;
 
-@WebServlet("/ping")
-public class Ping extends HttpServlet {
+
+@WebServlet("/")
+public class Default extends HttpServlet {
     private String message;
-    public void init() throws ServletException{
-        message = "ping";
+    public void init() throws ServletException {
+        message = "default";
     }
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Logger log=Logger.getLogger("xwisen");
-        log.info("mapping url /ping");
+        log.info("mapping url /");
         // 设置响应内容类型
         response.setContentType("text/html");
         // 实际的逻辑是在这里
